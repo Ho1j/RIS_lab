4a0847114a2baed09ed4006a7a0fd852f748040a
Mikhail <mike25122003@gmail.com>
2024-12-05 22:51:17 +0300

    Changed report, now it displays total orders by product for the period

--- blueprints/bp_reports/route.py
+++ blueprints/bp_reports/route.py
@@ -1,4 +1,5 @@
 from calendar import month
+from datetime import datetime
 
 from flask import Blueprint, render_template, request, current_app, session, redirect, flash, url_for
 from db_utils import execute_and_fetch
@@ -24,18 +25,24 @@ def create_1():
         if request.method == 'GET':
             return render_template('create_1.html')
         else:
-            month = request.form.get('month')
-            year = request.form.get('year')
-            find_check_count = provider.get_sql('check_1.sql', month=month, year=year)
+            date_start = request.form.get('date_start')
+            date_end = request.form.get('date_end')
+            if datetime.strptime(date_start, "%Y-%m-%d") > datetime.strptime(date_end, "%Y-%m-%d"):
+                flash('Дата начала периода не может быть меньше даты его конца', 'error')
+                return render_template('create_1.html')
+
+            find_check_count = provider.get_sql('create_total_orders_by_period_report.sql', date_start=date_start, date_end=date_end)
             check_count = execute_and_fetch(current_app.config['DB_CONFIG'], find_check_count)
+            print(check_count[0]['count'])
+            print(check_count)
             if check_count[0]['count'] == 0:
-                sql = provider.get_sql('create_report_1.sql', month=month, year=year)
+                sql = provider.get_sql('create_total_orders_by_period_report.sql.sql', date_start=date_start, date_end=date_end)
                 result = execute_and_fetch(current_app.config['DB_CONFIG'], sql)
                 if result is None:
-                    flash(f"Вы создали отчёт за {month} месяц {year} года", 'success')
+                    flash(f"Вы создали отчёт за период с {date_start} по {date_end}", 'success')
                     return redirect(url_for('bp_reports.reports_choice'))
                 else:
-                    flash(f"Нельзя создать отчёт на этот месяц и год", 'error')
+                    flash(f"Нельзя создать отчёт за период с {date_start} по {date_end}", 'error')
                     return render_template('create_1.html')
 
             else:
@@ -48,16 +55,16 @@ def create_1():
 @bp_reports.route('/view_1', methods=['GET', 'POST'])
 def view_1():
     if request.method == 'GET':
-        sql = provider.get_sql('view_reports_1.sql')
+        sql = provider.get_sql('view_all_total_orders_by_period_reports.sql')
         print(sql)
         reports_list = execute_and_fetch(current_app.config['DB_CONFIG'], sql)
         print(reports_list)
         return render_template('view_1.html', reports_list=reports_list)
     else:
-        month = request.form.get('month')
-        year = request.form.get('year')
-        sql = provider.get_sql('view_report_result_1.sql', month=month, year=year)
+        date_start = request.form.get('date_start')
+        date_end = request.form.get('date_end')
+        sql = provider.get_sql('select_total_orders_by_period_report.sql', date_start=date_start, date_end=date_end)
         result = execute_and_fetch(current_app.config['DB_CONFIG'], sql)
-        title = f"Отчёт за {month} месяц {year} года"
+        title = f"Отчёт за период с {date_start} по {date_end}"
         num = 1
         return render_template('reports_result.html', result=result, title=title, num=num)
