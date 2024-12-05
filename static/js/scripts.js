c5f6e6286ca410239e5fd3a92a0353f380f830e2
Mikhail <mike25122003@gmail.com>
2024-12-06 00:40:13 +0300

    new flashed messages style + js script

---
+++ static/js/scripts.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const flashMessages = document.querySelectorAll('.flash-message');
+    setTimeout(() => {
+        flashMessages.forEach(message => {
+            message.style.transition = 'opacity 0.5s';
+            message.style.opacity = '0';
+            setTimeout(() => message.remove(), 500); // Удалить элемент после анимации
+        });
+    }, 5000); // Сообщения исчезнут через 5 секунд
+});
